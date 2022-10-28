e40325f5438af47d7e0a5bad50b5f41852169390
Tainan Nery <zaoldyeck@outlook.com>
2022-10-27 22:37:13 -0300

    fix: code pattern

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   *, *::after, *::before {
@@ -15,14 +15,14 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme["green-500"]};
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
   }
 
   body {
     line-height: 1.5;
     -webkit-font-smoothing: antialised;
-    background-color: ${({ theme }) => theme["gray-900"]};
-    color: ${({ theme }) => theme["gray-300"]};
+    background-color: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
   }
 
   body, input, textarea, button {
